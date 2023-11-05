4c486fd04b35243962e3f072fd0be1c377d8a852
Louis <louis.yousif@yahoo.de>
2023-11-05 13:49:56 +0000

    defined model

--- Group addition/model.py
+++ Group addition/model.py
@@ -1,13 +1,21 @@
 import torch as t
-from transformer_lens import (
-    utils,
-    ActivationCache,
-    HookedTransformer,
-    HookedTransformerConfig,
-)
-from transformer_lens.hook_points import HookPoint
-from transformer_lens.components import LayerNorm
+import einops
 
-# Size of the set endowed with the group structure
 
-device = t.device("cuda" if t.cuda.is_available() else "cpu")
+class MLP(t.nn.Module):
+    def __init__(self, params):
+        super().__init__()
+        self.Embedding_left = t.nn.Embedding(params.N, params.embed_dim, bias=True)
+        self.Embedding_right = t.nn.Embedding(params.N, params.embed_dim, bias=True)
+        self.linear = t.nn.Linear(params.embed_dim * 2, params.hidden_size, bias=True)
+        self.activation = t.nn.GELU()
+        self.Umbedding = t.nn.Linear(params.hidden_size, params.N, bias=True)
+
+    def forward(self, a, b):
+        x1 = self.Embedding_left(a)
+        x2 = self.Embedding_right(b)
+        x12 = einops.rearrange([x1, x2], axis=-1)
+        hidden = self.linear(x12)
+        hidden = self.activation(hidden)
+        out = self.Umbedding(hidden)
+        return out
