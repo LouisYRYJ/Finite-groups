8803fc838203876e81b13f1b808c99f8b1ddf7d7
wilson <wiwu2390@colorado.edu>
2024-08-05 21:14:19 +0000

    MLP2 instancewise + llc_v_epoch notebook

--- src/model.py
+++ src/model.py
@@ -32,30 +32,30 @@ class MLP(nn.Module):
         return out
 
 
-class MLP2(nn.Module):
-    def __init__(self, params):
-        super().__init__()
-        self.embedding_left = nn.Embedding(params.N, params.embed_dim)
-        self.embedding_right = nn.Embedding(params.N, params.embed_dim)
-        self.linear_left = nn.Linear(params.embed_dim, params.hidden_size, bias=False)
-        self.linear_right = nn.Linear(params.embed_dim, params.hidden_size, bias=False)
-        if params.activation == "gelu":
-            self.activation = nn.GELU()
-        if params.activation == "relu":
-            self.activation = nn.ReLU()
-        self.unembedding = nn.Linear(params.hidden_size, params.N, bias=False)
+# class MLP2(nn.Module):
+#     def __init__(self, params):
+#         super().__init__()
+#         self.embedding_left = nn.Embedding(params.N, params.embed_dim)
+#         self.embedding_right = nn.Embedding(params.N, params.embed_dim)
+#         self.linear_left = nn.Linear(params.embed_dim, params.hidden_size, bias=False)
+#         self.linear_right = nn.Linear(params.embed_dim, params.hidden_size, bias=False)
+#         if params.activation == "gelu":
+#             self.activation = nn.GELU()
+#         if params.activation == "relu":
+#             self.activation = nn.ReLU()
+#         self.unembedding = nn.Linear(params.hidden_size, params.N, bias=False)
 
-    def forward(self, a):
+#     def forward(self, a):
 
-        a_1, a_2 = a[:, 0], a[:, 1]
-        x1 = self.embedding_left(a_1)
-        x2 = self.embedding_right(a_2)
-        hidden_x1 = self.linear_left(x1)
-        hidden_x2 = self.linear_right(x2)
-        hidden_sum = hidden_x1 + hidden_x2
-        hidden = self.activation(hidden_sum)
-        out = self.unembedding(hidden)
-        return out
+#         a_1, a_2 = a[:, 0], a[:, 1]
+#         x1 = self.embedding_left(a_1)
+#         x2 = self.embedding_right(a_2)
+#         hidden_x1 = self.linear_left(x1)
+#         hidden_x2 = self.linear_right(x2)
+#         hidden_sum = hidden_x1 + hidden_x2
+#         hidden = self.activation(hidden_sum)
+#         out = self.unembedding(hidden)
+#         return out
 
 
 def custom_xavier(dims):
@@ -139,9 +139,92 @@ class InstancedModule(ABC, nn.Module):
         # can't use einops rearrange here bc variable number of instances per ibatch
         return t.concat(out, dim=1)
 
+
+class MLP2(InstancedModule):
+    '''
+    Architecture used by Chughtai et al. and Stander et al.
+    '''
+    def __init__(self, N, params):
+        super().__init__()
+        self.params = params
+        self.N = N
+
+        self.embedding_left = custom_kaiming(
+            [params.instances, self.N, params.embed_dim]
+        )
+
+        self.embedding_right = custom_kaiming(
+            [params.instances, self.N, params.embed_dim]
+        )
+
+        self.linear_left = custom_kaiming(
+            [params.instances, params.embed_dim, params.hidden_size]
+        )
+
+        self.linear_right = custom_kaiming(
+            [params.instances, params.embed_dim, params.hidden_size]
+        )
+
+        self.unembedding = custom_kaiming(
+            [params.instances, params.hidden_size, self.N]
+        )
+
+        if params.activation == "gelu":
+            self.activation = nn.GELU()
+        elif params.activation == "relu":
+            self.activation = nn.ReLU()
+        elif params.activation == "linear":
+            self.activation = lambda x: x
+        else:
+            raise ValueError("Activation not recognized")
+
+    @jaxtyped(typechecker=beartype)
+    def _forward(
+        self, a: Int[t.Tensor, "batch_size entries"]
+    ) -> Float[t.Tensor, "batch_size instances d_vocab"]:
+
+        a_instances = einops.repeat(
+            a, " batch_size entries -> batch_size n entries", n=self.num_instances(),
+        )  # batch_size instances entries
+        a_1, a_2 = a_instances[..., 0], a_instances[..., 1]
+
+        a_1_onehot = F.one_hot(a_1, num_classes=self.N).float()
+        a_2_onehot = F.one_hot(a_2, num_classes=self.N).float()
+
+        x_1 = einops.einsum(
+            a_1_onehot,
+            self.embedding_left,
+            "batch_size instances d_vocab, instances d_vocab embed_dim -> batch_size instances embed_dim",
+        )
+        x_2 = einops.einsum(
+            a_2_onehot,
+            self.embedding_right,
+            "batch_size instances d_vocab, instances d_vocab embed_dim -> batch_size instances embed_dim",
+        )
+
+        hidden_1 = einops.einsum(
+            x_1,
+            self.linear_left,
+            "batch_size instances embed_dim, instances embed_dim hidden -> batch_size instances hidden",
+        )
+        hidden_2 = einops.einsum(
+            x_2,
+            self.linear_right,
+            "batch_size instances embed_dim, instances embed_dim hidden -> batch_size instances hidden",
+        )
+        hidden = hidden_1 + hidden_2
+
+        out = einops.einsum(
+            self.activation(hidden),
+            self.unembedding,
+            "batch_size instances hidden, instances hidden d_vocab-> batch_size instances d_vocab ",
+        )
+        return out
+
 class MLP3(InstancedModule):
-    # TODO: It's maybe better practice to explicitly list the args (instances, embed_dim, etc)
-    # instead of using the params object
+    '''
+    This architecture isn't used by any existing work. Our own invention :)
+    '''
     def __init__(self, N, params):
         super().__init__()
         self.params = params
