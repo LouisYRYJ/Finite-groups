a8f3cf85ec78b201ade6a2cb139be373d608ecdc
wwu <wiwu2390@colorado.edu>
2024-10-01 13:57:10 -0600

    anonymize

--- scripts/nhrun.sh
+++
@@ -1,2 +0,0 @@
-NAME=nohup_$(date +%Y%m%d-%H%M%S)
-nohup "$@" 1> $NAME.out 2> $NAME.err & echo $! > $NAME.pid
--- scripts/poetry_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-apt-get -y update && apt-get -y install gap   # needed for gappy
-pip install poetry
-# poetry config virtualenvs.in-project true
-poetry install
-poetry shell
