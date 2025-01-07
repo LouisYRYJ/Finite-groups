22a8eb94e4230f1a08fa43457aabffaab41193ff
wilson <wiwu2390@colorado.edu>
2025-01-07 23:22:05 +0000

    update pyproject.toml and poetry_setup.sh

--- scripts/poetry_setup.sh
+++ scripts/poetry_setup.sh
@@ -3,4 +3,5 @@ apt-get -y update && apt-get -y install gap   # needed for gappy
 pip install poetry
 # poetry config virtualenvs.in-project true
 poetry install
+poetry self add poetry-plugin-shell
 poetry shell
