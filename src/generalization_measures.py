9faab7538768c48801394c63cfe3e15c74f67b7d
wilson <wiwu2390@colorado.edu>
2025-01-17 02:33:35 +0000

    generalization measures skeleton

---
+++ src/generalization_measures.py
@@ -0,0 +1,103 @@
+# Implementations of measures described in "Fantastic Generalization Measures and Where to Find Them" by Jiang et al.
+# All measures should take (models, train_data) as input
+# where models is an InstancedModule, one instance per training step
+# and returns one scalar per instance
+
+def cross_entropy(models, train_data):
+    pass
+
+def inv_margin(models, train_data):
+    pass
+
+def neg_entropy(models, train_data):
+    pass
+
+def spec_init(models, train_data):
+    pass
+
+def spec_orig(models, train_data):
+    pass
+
+def spec_init_main(models, train_data):
+    pass
+
+def spec_orig_main(models, train_data):
+    pass
+
+def prod_of_spec_margin(models, train_data):
+    pass
+
+def prod_of_spec(models, train_data):
+    pass
+
+def fro_spec(models, train_data):
+    pass
+
+def sum_of_spec_margin(models, train_data):
+    pass
+
+def sum_of_spec(models, train_data):
+    pass
+
+def prod_of_fro_margin(models, train_data):
+    pass
+
+def sum_of_fro(models, train_data):
+    pass
+
+def frobenius_distance(models, train_data):
+    pass
+
+def dist_spec_init(models, train_data):
+    pass
+
+def param_norm(models, train_data):
+    pass
+
+def path_norm_margin(models, train_data):
+    pass
+
+def path_norm(models, train_data):
+    pass
+
+def fisher_rao(models, train_data):
+    pass
+
+def pac_bayes_init(models, train_data):
+    pass
+
+def pac_bayes_orig(models, train_data):
+    pass
+
+def sharpness_init(models, train_data):
+    pass
+
+def sharpness_orig(models, train_data):
+    pass
+
+def pac_bayes_flatness(models, train_data):
+    pass
+
+def sharpness_flatness(models, train_data):
+    pass
+
+def pac_bayes_mag_init(models, train_data):
+    pass
+
+def pac_bayes_mag_orig(models, train_data):
+    pass
+
+def pac_sharpness_mag_init(models, train_data):
+    pass
+
+def pac_sharpness_mag_orig(models, train_data):
+    pass
+
+def pac_bayes_mag_flat(models, train_data):
+    pass
+
+def pac_sharpness_mag_flat(models, train_data):
+    pass
+
+def grad_noise(models, train_data):
+    pass
